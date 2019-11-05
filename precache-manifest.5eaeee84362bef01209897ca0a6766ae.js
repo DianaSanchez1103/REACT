5ad77f8f72c7819039dd3fc5df32404feab86394
DianaSanchez1103 <00188618@uca.edu.sv>
2019-11-05 16:22:49 -0600

    Updates

---
+++ precache-manifest.5eaeee84362bef01209897ca0a6766ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d70132b2ea6fbe9e77a929ea3637b7a",
+    "url": "/reacts/index.html"
+  },
+  {
+    "revision": "53e58bb72f1f064d4730",
+    "url": "/reacts/static/css/main.318cce55.chunk.css"
+  },
+  {
+    "revision": "7e3af4f2ba30a37819a4",
+    "url": "/reacts/static/js/2.92a6222c.chunk.js"
+  },
+  {
+    "revision": "53e58bb72f1f064d4730",
+    "url": "/reacts/static/js/main.409be5fa.chunk.js"
+  },
+  {
+    "revision": "ccef158d3011f1160372",
+    "url": "/reacts/static/js/runtime-main.4b19bb24.js"
+  }
+]);
